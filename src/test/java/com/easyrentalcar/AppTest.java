4f62b930555d7e137c4683b24a17abb1c22df800
Anna Rozycka <annjoe@vp.pl>
2018-11-19 20:01:51 +0100

    add new classes and tests

--- src/test/java/com/easyrentalcar/AppTest.java
+++ src/test/java/com/easyrentalcar/AppTest.java
@@ -1,13 +1,8 @@
 package com.easyrentalcar;
 
-
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
