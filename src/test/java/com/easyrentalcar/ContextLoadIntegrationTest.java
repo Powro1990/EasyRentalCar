13ad9ad282b3348c2fd1bedf680cf29e4bd436cb
Anna Rozycka <annjoe@vp.pl>
2018-11-21 18:57:36 +0100

    Add integration test to verify if spring container initializes correctly

---
+++ src/test/java/com/easyrentalcar/ContextLoadIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.easyrentalcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringJUnitConfig
+@SpringBootTest
+public class ContextLoadIntegrationTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @DisplayName("should initialize Spring context without exceptions")
+    @Test
+    void test() throws Exception {
+        assertThat(context).isNotNull();
+    }
+}
