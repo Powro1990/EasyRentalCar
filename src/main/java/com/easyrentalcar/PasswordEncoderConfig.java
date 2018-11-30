7d9dbe10f7930f235d327444a33fb1ecdfd855b1
Anna Rozycka <annjoe@vp.pl>
2018-11-30 18:21:33 +0100

    Enable MySQL on production

--- src/main/java/com/easyrentalcar/PasswordEncoderConfig.java
+++ src/main/java/com/easyrentalcar/PasswordEncoderConfig.java
@@ -2,6 +2,7 @@ package com.easyrentalcar;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
